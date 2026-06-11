ca2bb0ab3a46b41364b51141030b0eff1441b392
MaxD-Sheridan <dabski@sheridancollege.ca>
2026-06-11 11:29:06 -0400

    added supabase module to backend; added handling of plural words

--- backend/controllers/missingIngredientsController.js
+++ backend/controllers/missingIngredientsController.js
@@ -2,11 +2,23 @@ const { fetchFromEdamam } = require('../services/edamamService');
 const { buildIngredientSearchUrlWithRecipeId, edamamAccountUser } = require('../config');
 
 // Fetch ingredients THAT DO NOT CONTAIN QUERY PARAMS from the Edamam API and return to the client
-// TODO: Fix rawIngredients not being array (or maybe frontend needs to add recipe ID?)
+// TODO: Add support for multiple ingredients (maybe in a comma-separated format i.e. "tomato,onion,garlic")
 async function getMissingIngredients(req, res) {
     // Require and validate recipe ID and query parameter(s)
-    const recipeId = String(req.query?.recipe_id || '').trim();
-    const ingredient_query = String(req.query?.q || '').trim().toLowerCase();
+    const recipeId = String(req.query?.recipe_id || '').trim(); // Edamam recipe ID
+    const ingredient_query = String(req.query?.q || '') // User inputted search term
+        .trim()
+        .toLowerCase()
+
+    // Handle plural suffixes in ingredient query string
+    if (ingredient_query.endsWith('s')) {
+        if (ingredient_query.endsWith('es')) {
+            ingredient_query = ingredient_query.slice(0, -2);
+        } else {
+            ingredient_query = ingredient_query.slice(0, -1);
+        }
+    }
+
     if (!ingredient_query || !recipeId) {
         return res.status(400).send('Bad Request: use "/missing-ingredients?recipe_id=[recipeId]&q=[ingredients]"');
     }
